ca7d54d11da0444176c4c860b11b777c2a8cda75
Abraham Rivera (Cod3f1re) <abraham.rivera@circulodecredito.com.mx>
2024-02-28 14:15:17 -0600

    Initial Commit

---
+++ Example interview/Example_interviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Example_interviewApp.swift
+//  Example interview
+//
+//  Created by Abraham Rivera Rojas on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Example_interviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
